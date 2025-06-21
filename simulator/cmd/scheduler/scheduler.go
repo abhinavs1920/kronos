6ed4c9742a796676de1d2ea9b2e3849dbfa59bdb
Abhinav Sharma <abhinavs1920bpl@gmail.com>
2025-06-21 15:59:40 +0530

    chore: add custom scheduler spec

---
+++ simulator/cmd/scheduler/scheduler.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"k8s.io/component-base/cli"
+	_ "k8s.io/component-base/logs/json/register" // for JSON log format registration
+	_ "k8s.io/component-base/metrics/prometheus/clientgo"
+	_ "k8s.io/component-base/metrics/prometheus/version" // for version metric registration
+	"k8s.io/klog"
+	"sigs.k8s.io/kube-scheduler-simulator/simulator/pkg/debuggablescheduler"
+	"sigs.k8s.io/kube-scheduler-simulator/simulator/plugins/energyplugin"
+)
+
+func main() {
+	command, cancelFn, err := debuggablescheduler.NewSchedulerCommand(
+		debuggablescheduler.WithPlugin("energyplugin", energyplugin.New),
+	)
+	if err != nil {
+		klog.Info(fmt.Sprintf("failed to build the debuggablescheduler command: %+v", err))
+		os.Exit(1)
+	}
+	code := cli.Run(command)
+
+	cancelFn()
+	os.Exit(code)
+}
